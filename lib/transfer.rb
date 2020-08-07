b152a1e865e56b93a0e44832e914f6a7f65acd8e
kain <kaindoneson@gmail.com>
2020-08-07 11:10:18 -0400

    Was a pleasure working Soroosh

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,34 @@
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :status, :amount;
+
+  def initialize(sender, receiver, amount)
+    @sender = sender;
+    @receiver = receiver;
+    @amount = amount;
+    @status = 'pending';
+  end
+
+  def valid?
+     self.sender.valid? and self.receiver.valid?
+  end
+
+  def execute_transaction
+    if self.valid? and self.sender.balance >= self.amount and self.status == 'pending'
+      self.receiver.balance += self.amount;
+      self.sender.balance -= self.amount;
+      self.status = 'complete';
+    else
+      self.status = 'rejected'
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+    def reverse_transfer
+      if self.status == 'complete' and self.valid?
+        self.receiver.balance -= self.amount;
+        self.sender.balance += self.amount;
+        self.status = 'reversed';
+      end
+    end
 end
